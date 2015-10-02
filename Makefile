6c263a49655ca75bc0a50d8eb334d5d88d03b075
fitnr <fitnr@fakeisthenewreal>
2015-10-02 00:46:56 -0400

    add/edit license

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
+# Licensed under the General Public License (version 3)
+# http://opensource.org/licenses/LGPL-3.0
+# Copyright (c) 2015, Neil Freeman <contact@fakeisthenewreal.org>
+
 readme.rst: readme.md
 	pandoc $< -o $@
--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+# Tweetcal: Convert a tweet stream to ics calendar
+# Copyright (c) 2014-2015 Neil Freeman
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 from setuptools import setup
 
 try:
@@ -21,7 +31,16 @@ setup(
 
     author_email='contact@fakeisthenewreal.org',
 
-    license='GPLv3',
+    license='GPL-3.0',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+    ],
 
     packages=['tweetcal'],
 
